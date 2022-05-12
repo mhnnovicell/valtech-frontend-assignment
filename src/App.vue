ee1ee0b540010150730f9151ce8f31ebccf366b5
mninozebra <mni@nozebra.dk>
2022-05-12 13:41:00 +0200

    Cleaning up project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app" class="container flex flex-col">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ProductList></ProductList>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import HelloWorld from './components/HelloWorld';
+import ProductList from './views/ProductList';
 
 // HelloWorld class will be a Vue component
 @Component({
@@ -16,19 +15,8 @@ import HelloWorld from './components/HelloWorld';
   // See Vue.js docs for all available options:
   // https://vuejs.org/v2/api/#Options-Data
   components: {
-    HelloWorld,
+    ProductList,
   },
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/ProductList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>hello from productlist</p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default class ProductList extends Vue {}
+</script>
