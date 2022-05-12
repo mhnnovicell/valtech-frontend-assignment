a0bd2390f4d11dffdddbc319a5929ff4be95f194
mninozebra <mni@nozebra.dk>
2022-05-12 13:28:52 +0200

    Adding tailwind and Vue class components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container flex flex-col">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import HelloWorld from './components/HelloWorld';
 
-export default {
-  name: 'App',
+// HelloWorld class will be a Vue component
+@Component({
+  // Specify `components` option.
+  // See Vue.js docs for all available options:
+  // https://vuejs.org/v2/api/#Options-Data
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
